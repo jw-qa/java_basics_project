dbf9859a476407836d1ea02be49ed45ae5f9047a
Jake Wallace <wallacejake6@gmail.com>
2022-10-19 14:54:05 +0100

    Completed a VAT exercise utilising the Scanner class

--- src/main/java/com/lbg/coh2/App.java
+++ src/main/java/com/lbg/coh2/App.java
@@ -3,6 +3,7 @@ package com.lbg.coh2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -12,12 +13,17 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Enter a message: " );
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String dataRead = br.readLine(); 
-        
-        System.out.println("=> " + dataRead);
+    	System.out.println("Enter the cost of the item: ");
+    	Scanner console = new Scanner(System.in);
+    	double cost = console.nextDouble();
+    	System.out.println("Enter the VAT: ");
+    	double vat = console.nextDouble();
+    	App app = new App();
+    	
+    	System.out.println("The total is: " + app.VAT(cost, vat));
+    }
+    
+    double VAT (double cost, double vat) {
+    	return (cost * (1 + vat / 100));
     }
 }
