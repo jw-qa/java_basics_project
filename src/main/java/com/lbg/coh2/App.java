eff2d04c5bad2621a1032af1327c70b7f9968f3e
Jake Wallace <wallacejake6@gmail.com>
2022-10-19 16:26:15 +0100

    Calculates total VAT for user input of prices. Prints running total

--- src/main/java/com/lbg/coh2/App.java
+++ src/main/java/com/lbg/coh2/App.java
@@ -1,8 +1,6 @@
 package com.lbg.coh2;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -13,14 +11,28 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-    	System.out.println("Enter the cost of the item: ");
-    	Scanner console = new Scanner(System.in);
-    	double cost = console.nextDouble();
-    	System.out.println("Enter the VAT: ");
-    	double vat = console.nextDouble();
     	App app = new App();
+    	double total=0;
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter the VAT percentage: ");
+    	double vat = sc.nextDouble();
+    	String lineRead = "x";
+    	boolean whileContinue = true;
     	
-    	System.out.println("The total is: " + app.calculateVAT(cost, vat));
+    	while ( whileContinue && lineRead != null) {
+    		System.out.println("Enter the cost of the item or type 'quit' to EXIT");
+    	
+    		lineRead = sc.next(); 
+    		
+    		if (lineRead.equalsIgnoreCase("quit")) {
+    			whileContinue = false;
+    		}
+    		else {
+    			total += app.calculateVAT(Double.parseDouble(lineRead), vat);
+    			System.out.println("Total is: " + total);
+    		}
+    		
+    	}
     }
     
     double calculateVAT (double cost, double vat) {
